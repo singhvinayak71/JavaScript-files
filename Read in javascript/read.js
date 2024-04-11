aa0dfe97bc199719b68d7670b0ebb9499596932e
Vinayak <singhvinayak7111@gmail.com>
2024-04-11 07:53:24 +0530

    read with dom

---
+++ Read in javascript/read.js
@@ -0,0 +1,8 @@
+// In javascript, there are several methods for reading data from various sources
+
+// 1. Reading from the DOM
+const elementId = document.getElementById("ID");
+const elementsByClass = document.getElementsByClassName("CLASS NAME");
+const elementsByTag = document.getElementsByTagName("div");
+const querySelector = document.querySelector("#ID");
+
